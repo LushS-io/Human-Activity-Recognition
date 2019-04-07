98e2a1bf01c26566fa1a3fe8908f4b0db975d9cd
Christopher Roberts <roberts.chris245@gmail.com>
2019-04-06 18:56:45 -0700

    created DF with labels and numbers seperated

--- EDA.py
+++ EDA.py
@@ -3,3 +3,16 @@ import pandas as pd
 import numpy as np
 
 # %%
+labelstxt = pd.read_csv(filepath_or_buffer="./HAPT Data Set/activity_labels.txt", names=['label'])
+
+labels = pd.DataFrame(columns=['labels', 'numbers'])
+labels['labels'] = labelstxt['label'].apply(lambda x: [str(x).strip(' 123456789')])
+labels['numbers'] = labelstxt['label'].apply(lambda x: [str(x).strip('ABCDEFGHIJKLMNOPQRSTUVWXYZ_ ')])
+print(labels)
+
+# %%
+df_features_info = pd.read_csv(filepath_or_buffer='./HAPT Data Set/features.txt')
+
+
+
+#%%
